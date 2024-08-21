722a001fb0470b7d9b99bb748bda4d49c4afc347
MOON <moonjs1018@naver.com>
2024-08-21 22:21:19 +0900

    innit commit

---
+++ src/main/java/com/sparta/upgradeschedulemanagement/UpgradeScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.upgradeschedulemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradeScheduleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpgradeScheduleManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/upgradeschedulemanagement/UpgradeScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.upgradeschedulemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpgradeScheduleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
